f31abddb976b05770b4e7257d3c2588d7ccddcdd
huanghao <huang@hao>
2020-12-13 09:36:07 +0800

    dddd

---
+++ src/main/java/com/example/exercise/chapter1/StackStructure.java
@@ -0,0 +1,9 @@
+package com.example.exercise.chapter1;
+
+public class StackStructure {
+    public static void main(String[] args) {
+        int i=2;
+        int j=3;
+        int k=i+j;
+    }
+}
